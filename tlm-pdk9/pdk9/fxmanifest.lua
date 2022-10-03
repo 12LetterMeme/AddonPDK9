ea71ec38622cd33289fd4d7b2b226880201bcea9
12LetterMeme <91357757+12LetterMeme@users.noreply.github.com>
2022-10-03 12:12:58 -0400

    update manifest

---
+++ tlm-pdk9/pdk9/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Manifest
+fx_version 'cerulean'
+games {'gta5'}
+
+description 'Add-on Police K-9 Ped'
+author '12LetterMeme#0001'
+version "1.0.0"
+
+files {
+'peds.meta',
+}
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
